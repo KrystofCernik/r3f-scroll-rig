e67bf3dd7f17f32199d0a2848462f119baa16a4e
Krystof Cernik <96994380+KrystofCernik@users.noreply.github.com>
2023-07-27 14:04:36 +0200

    initial commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,123 @@
+import { useState, useRef, useEffect } from 'react'
+import * as THREE from 'three'
+import { Environment, useTexture, MeshPortalMaterial, RoundedBox, Text, CameraControls, useCursor } from '@react-three/drei'
+import { easing } from 'maath'
+import { useFrame, useThree } from '@react-three/fiber'
+import Fish from './Fish'
+import Dragon from './Dragon'
+import Cactoro from './Cactoro'
+
+export const Experience = () => {
+	const [active, setActive] = useState(null)
+	const [hovered, setHovered] = useState(null)
+	useCursor(hovered)
+	const controls = useRef()
+	const scene = useThree((state) => state.scene)
+
+	useEffect(() => {
+		if (active) {
+			const targetPosition = new THREE.Vector3()
+			scene.getObjectByName(active).getWorldPosition(targetPosition)
+			controls.current.setLookAt(
+				0,
+				0,
+				5,
+				targetPosition.x,
+				targetPosition.y,
+				targetPosition.z,
+				true
+			)
+		} else {
+			controls.current.setLookAt(
+				0,
+				0,
+				10,
+				0,
+				0,
+				0,
+				true
+			)
+		}
+	}, [active])
+
+	return (
+		<>
+			<ambientLight intensity={0.5} />
+			<Environment preset="sunset" />
+			<CameraControls ref={controls} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 6} />
+			<MonsterStage
+				texture={'/textures/water-env.jpg'}
+				name="Fish"
+				color={'#69CACE'}
+				position-z={-0.4}
+				active={active}
+				setActive={setActive}
+				hovered={hovered}
+				setHovered={setHovered}
+			>
+				<Fish scale={0.6} position-y={-1} active={active === 'Fish'} hovered={hovered === 'Fish'} />
+			</MonsterStage>
+			<MonsterStage
+				texture={'/textures/lava-env.jpg'}
+				name='Dragon'
+				color={'#78374D'}
+				position-x={-2.5}
+				rotation-y={Math.PI / 8}
+				active={active}
+				setActive={setActive}
+				hovered={hovered}
+				setHovered={setHovered}
+			>
+				<Dragon scale={0.6} position-y={-1} active={active === 'Dragon'} hovered={hovered === 'Dragon'} />
+			</MonsterStage>
+			<MonsterStage
+				texture={'/textures/dessert-env.jpg'}
+				name='Cactoro'
+				color={'#B57139'}
+				position-x={2.5}
+				rotation-y={-Math.PI / 8}
+				active={active}
+				setActive={setActive}
+				hovered={hovered}
+				setHovered={setHovered}
+			>
+				<Cactoro scale={0.5} position-y={-1} active={active === 'Cactoro'} hovered={hovered === 'Cactoro'} />
+			</MonsterStage>
+		</>
+	)
+}
+
+const MonsterStage = ({children, texture, name, color, active, setActive, hovered, setHovered, ...props}) => {
+
+	const map = useTexture(texture)
+	const portal = useRef()
+
+	useFrame((_state, delta) => {
+		const worldOpen = active === name
+		easing.damp(portal.current, 'blend', worldOpen ? 1 : 0, 0.2, delta)
+	})
+
+	return <group {...props}>
+		<Text font='/fonts/Dosis-SemiBold.ttf' fontSize={0.3} position={[0, -1.3, 0.06]} anchorY='bottom'>
+			{name}
+			<meshBasicMaterial color={color} toneMapped={false} />
+		</Text>
+		<RoundedBox
+			name={name}
+			args={[2, 3, 0.1]}
+			onPointerEnter={() => setHovered(name) }
+			onPointerLeave={() => setHovered(null) }
+			onClick={() => setActive(active === name ? null : name)}
+		>
+			<MeshPortalMaterial ref={portal} side={THREE.DoubleSide}>
+				<ambientLight intensity={0.5} />
+				<Environment preset="sunset" />
+				{children}
+				<mesh>
+					<sphereGeometry args={[5, 64, 64]} />
+					<meshStandardMaterial map={map} side={THREE.BackSide} />
+				</mesh>
+			</MeshPortalMaterial>
+		</RoundedBox>
+	</group>
+}
